7e891630b358891e6f436d03c3b2645776580db7
aneefdev <anamanif1@gmail.com>
2025-03-18 23:28:44 +0700

    Add Components React

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="flex text-center px-4 py-8 justify-center">
+      <p className="text-sm">
+        Copyright 2025 • All Rights Reserved LuxSpace by AneefDev
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
