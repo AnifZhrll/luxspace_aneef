86eea08ee478905ae6d860e7729191fe1fc8e9c5
aneefdev <anamanif1@gmail.com>
2025-05-02 14:40:05 +0700

    Offline info and manifest

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import Arrived from './components/Arrived';
 import Client from './components/Client';
 import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
+import Offline from './components/Offline';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  function handleOfflineStatus() {
+    setOfflineStatus(!navigator.onLine);
+  }
 
   React.useEffect(function() {
     (async function () {
@@ -21,10 +27,32 @@ function App() {
       });
       const { nodes } = await response.json();
       setItems(nodes);
+
+      if (!document.querySelector('script[src="/carousel.js"]')) {
+
+        const script = document.createElement("script");
+
+        script.src = "/carousel.js";
+
+        script.async = false;
+
+        document.body.appendChild(script);
+
+      };
     })();
-  }, []);
+
+    handleOfflineStatus();
+    window.addEventListener('online', handleOfflineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
+
+    return function () {
+      window.removeEventListener('online', handleOfflineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
+    }
+  }, [offlineStatus]);
   return (
     <>
+    {offlineStatus && <Offline />}
      <Header />
      <Hero />
      <Browse />
