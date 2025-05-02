5754cc5a09088c2365248c6631c9fadc45535d1d
aneefdev <anamanif1@gmail.com>
2025-05-02 15:11:58 +0700

    Add Splash Screen

--- src/App.js
+++ src/App.js
@@ -1,67 +1,81 @@
-import React from 'react';
-import './tailwind.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Browse from './components/Browse';
-import Arrived from './components/Arrived';
-import Client from './components/Client';
-import AsideMenu from './components/AsideMenu';
-import Footer from './components/Footer';
-import Offline from './components/Offline';
+import React from "react";
+import "./tailwind.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Browse from "./components/Browse";
+import Arrived from "./components/Arrived";
+import Client from "./components/Client";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
+import Offline from "./components/Offline";
+import Splash from "./pages/Splash";
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   function handleOfflineStatus() {
     setOfflineStatus(!navigator.onLine);
   }
 
-  React.useEffect(function() {
-    (async function () {
-      const response = await fetch('https://bwacharity.fly.dev/items', {
-        headers: {
-          "Content-Type": "application/json",
-          "accept": "application/json",
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch("https://bwacharity.fly.dev/items", {
+          headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+          },
+        });
+        const { nodes } = await response.json();
+        setItems(nodes);
+
+        if (!document.querySelector('script[src="/carousel.js"]')) {
+          const script = document.createElement("script");
+
+          script.src = "/carousel.js";
+
+          script.async = false;
+
+          document.body.appendChild(script);
         }
-      });
-      const { nodes } = await response.json();
-      setItems(nodes);
+      })();
 
-      if (!document.querySelector('script[src="/carousel.js"]')) {
+      handleOfflineStatus();
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
 
-        const script = document.createElement("script");
+      setTimeout(function () {
+        setIsLoading(false);
+      }, 1500);
 
-        script.src = "/carousel.js";
 
-        script.async = false;
-
-        document.body.appendChild(script);
-
+      return function () {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
       };
-    })();
-
-    handleOfflineStatus();
-    window.addEventListener('online', handleOfflineStatus);
-    window.addEventListener('offline', handleOfflineStatus);
-
-    return function () {
-      window.removeEventListener('online', handleOfflineStatus);
-      window.removeEventListener('offline', handleOfflineStatus);
-    }
-  }, [offlineStatus]);
+    },
+    [offlineStatus]
+  );
   return (
     <>
-    {offlineStatus && <Offline />}
-     <Header />
-     <Hero />
-     <Browse />
-     <Arrived items={items}/>
-     <Client />
-     <AsideMenu />
-     <Footer />
+      {isLoading === true ? (
+        <Splash />
+      ) : (
+        <>
+          {offlineStatus && <Offline />}
+          <Header />
+          <Hero />
+          <Browse />
+          <Arrived items={items} />
+          <Client />
+          <AsideMenu />
+          <Footer />
+        </>
+      )}
     </>
   );
-};
+}
 
 export default App;
